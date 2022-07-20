ef5ff4cf6b566e579e261b3eabd6e6d6da0ff207
MindaugasPuz <96083581+MindaugasPuzinauskas@users.noreply.github.com>
2022-07-20 21:58:22 +0300

    Tkinter frame added

--- project.py
+++ project.py
@@ -1,9 +1,67 @@
 from tkinter import *
 import tkinter
+from tkinter import ttk
 
-#Formule skirta apskaiciuoti zmogaus Kuno Mases indeksa
-def kmi(self):
+class skaiciuokles:
+    def __init__(self,langas):
+        notebook_tab=ttk.Notebook(langas)
+        notebook_tab.pack()
 
+        frame1= Frame(notebook_tab, width=500, height=500, bg="grey")
+        frame2= Frame(notebook_tab, width=500, height=500, bg="grey")
+        frame3=Frame(notebook_tab, width=500, height=500, bg="grey")
+        frame4=Frame(notebook_tab, width=500, height=500, bg="grey")
+
+        frame1.pack(fill="both", expand=1)
+        frame2.pack(fill="both", expand=1)
+        frame3.pack(fill="both", expand=1)
+        frame4.pack(fill="both", expand=1)
+
+
+        notebook_tab.add(frame1, text="KMI Skaičiuoklė")
+        notebook_tab.add(frame2, text="KCAL Skaičiuoklė")
+        notebook_tab.add(frame3, text="Kiek reikia išgerti vandens?")
+        notebook_tab.add(frame4, text="Maisto produktų KCAL skaičiuoklė")
+
+        vcmd=langas.register(self.validate)
+        question1= Label(frame1, text="Įveskite savo svorį kilogramais: ", justify=CENTER, anchor='w')
+        question1.grid(sticky=W, column=0, row=0)
+
+        self.svoris= Entry(frame1, validate="key",validatecommand=(vcmd,'%P'))
+        self.svoris.grid(column=0, row=2)
+
+        question2= Label(frame1, text="Įveskite savo ūgį centimetrais: ", justify=CENTER, anchor='w')
+        question2.grid(sticky=W, column=0, row=4)
+
+        self.ugis= Entry(frame1, validate="key",validatecommand=(vcmd,'%P'))
+        self.ugis.grid(column=0, row=5)
+
+        self.mygtukas1= tkinter.Button(frame1, text= "Skaičiuoti KMI", command=lambda: self.kmi())
+        self.mygtukas1.grid(column=0,row=8, sticky='E') 
+
+        self.message=""
+        #Sukuriamas variable
+        self.label_text = StringVar()
+        #Set nustato, koks tekstas rodomas ka tik atsidarius lentele
+        self.label_text.set(self.message)
+        #Padarom, kad teskstas butu matomas lenteleje ir kad ji butu galima pakeisti
+        self.label = Label(frame1, textvariable=self.label_text)
+        self.label.grid(row=9,columnspan=5, sticky="W")
+    
+    def validate(new_text):
+        #leidzia istrinti entry reiksme
+        if not new_text: 
+            new_text = None
+            return True
+        #leidzia ivesti tik sveikus skaicius
+        if new_text:
+            try:
+             float(new_text)
+             return True
+            except ValueError:
+             return False
+    def kmi(self):
+        #si funkcijos dalis skirta identifikuoti neivestas reiksmes
         if len(self.svoris.get())==0 and len(self.ugis.get())==0:
             self.message="Neįvestas ūgis ir svoris"
         elif len(self.svoris.get())==0:
